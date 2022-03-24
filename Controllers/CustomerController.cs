1c0d31a7c0137c6a4b45c11f790067fdafc4c09b
jc080 <jc080@LAPTOP-5O6H8OMF>
2022-03-24 22:08:39 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TM2Multitier.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private CustomerBL customerBL;
+        public CustomerController()
+        {
+            customerBL = new CustomerBL();
+        }
+        public IActionResult Index()
+        {
+            var customer = customerBL.GetCustomer();
+            return View(customer);
+        }
+    }
+}
